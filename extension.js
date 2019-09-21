0a281fe70917f82fb5d598a01375f7e94ce6ed85
Max Rensen <40267916+Max-Rensen@users.noreply.github.com>
2019-09-21 11:58:37 +0200

    More fixes

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const vscode = require('vscode');
 const tab = " ".repeat(vscode.workspace.getConfiguration("editor", null).get("tabSize"));
 
+// All the bug fixes have made this code horrible and I am too lazy to fix it, please don't look here
 vscode.languages.registerDocumentFormattingEditProvider("assembly", {
 	provideDocumentFormattingEdits(document) {
 		const commentSpacing = vscode.workspace.getConfiguration("asmsupport").get("commentSpacing");
@@ -10,24 +11,27 @@ vscode.languages.registerDocumentFormattingEditProvider("assembly", {
 		let longestInstruction = 0;
 		for (let i = 0; i < document.lineCount; i++) {
 			const line = document.lineAt(i);
-			const label = /\w+:|\.\w+/.test(line.text);
+			const text = line.text.trim();
+			const label = /\w+:/.test(text);
+			const section = /\.\w+/.test(text);
 
 			let lineObject = {};
 			lineObject["range"] = line.range;
 			lineObject["label"] = label;
+			lineObject["section"] = section;
 
-			if (/[^#\W]+.*#/.test(line.text)) {
-				const index = line.text.indexOf("#");
-				const comment = line.text.substring(index + 1, line.text.length).trim();
-				const text = line.text.substring(0, index).trim();
+			if (/[^#]+.*#/.test(text)) {
+				const index = text.indexOf("#");
+				const comment = text.substring(index, text.length).trim();
+				const other = text.substring(0, index).trim();
 
 				lineObject["comment"] = comment;
-				lineObject["text"] = text;
+				lineObject["text"] = other;
 			} else {
-				lineObject["text"] = line.text.trim();
+				lineObject["text"] = text;
 			}
 
-			if (!label) {
+			if (!label && !section) {
 				const instruction = lineObject["text"].split(" ")[0];
 				lineObject["text"] = lineObject["text"].substring(instruction.length).trim();
 
@@ -36,23 +40,29 @@ vscode.languages.registerDocumentFormattingEditProvider("assembly", {
 				}
 
 				lineObject["instruction"] = instruction;
+			} else if (label) {
+				lineObject["labelSpacing"] = line.text.length - line.text.trimLeft().length;
 			}
 
-			if (line.text.trim().startsWith("#")) {
-				edits.push(vscode.TextEdit.replace(line.range, "# " + line.text.trim().substring(1).trimLeft()));
+			if (text.startsWith("#")) {
+				edits.push(vscode.TextEdit.replace(line.range, text));
 			} else {
 				lines.push(lineObject);
 			}
 		}
 
 		let longestComment = 0;
+		let isInstruction = false;
 		for (let line of lines) {
 			if (!line["comment"]) {
 				continue;
 			}
 
-			const length = (line["label"] ? 0 : tab.length) + line["text"].length + 1;
+			const spacing = line["label"] ? line["labelSpacing"] : (line["section"] ? 0 : tab.length);
+
+			const length = spacing + line["text"].length + ((line["label"] || line["section"]) ? 0 : 1);
 			if (length > longestComment) {
+				isInstruction = line["instruction"] != null;
 				longestComment = length;
 			}
 		}
@@ -63,17 +73,19 @@ vscode.languages.registerDocumentFormattingEditProvider("assembly", {
 
 			if (empty && instruction.length == 0) {
 				edits.push(vscode.TextEdit.replace(line["range"], ""));
+				continue;
 			}
 
 			const text =
-				line["label"] ?
-					line["text"] :
-					empty ? tab + instruction :
-						tab + instruction + " ".repeat(longestInstruction - instruction.length + 1) + line["text"];
+				line["label"] ? " ".repeat(line["labelSpacing"]) + line["text"] :
+					line["section"] ? line["text"] :
+						empty ? tab + instruction :
+							tab + instruction + " ".repeat(longestInstruction - instruction.length + 1) + line["text"];
 
+			const instructionLength = (line["instruction"] || ((line["label"] || line["section"]) && empty)) ? (isInstruction ? longestInstruction : 0) : 0;
 			const extra =
 				line["comment"] ?
-					" ".repeat(longestComment - text.length + longestInstruction + commentSpacing) + "# " + line["comment"] :
+					" ".repeat(longestComment - text.length + instructionLength + commentSpacing) + line["comment"] :
 					"";
 
 			edits.push(vscode.TextEdit.replace(line["range"], text + extra));
